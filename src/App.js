11f308a3eaa89ae3a33c7453fc2de8d842a00807
WarWarKyaw <warwar67890@gmail.com>
2023-03-21 09:11:21 +0630

    useState with object

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCouner from './components/HookCouner';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './HookCounterThree';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">    
       {/* <ClassCounter /> */}
       {/* <HookCouner /> */}
-      <HookCounterTwo />
+      {/* <HookCounterTwo /> */}
+      <HookCounterThree />
     </div>
   );
 }
